3957bcdc6708154a1de037e93dd620aaddc7c674
Sky Yong <jinghao0958@email.com>
2025-11-10 22:44:13 +0800

    button and link class from next/link and /components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const products  = await stripe.products.list({
@@ -9,6 +11,19 @@ export default async function Home() {
   });
   console.log("Home()",products);
   return (
-    <div></div>
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to My Ecommerce Store</h2>
+            <p>Discover our exclusive collection of products.</p>
+            <Button asChild variant="default">
+              
+              <Link href="/products">Browse all products</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
