de9cee0da94ce18ef5de5dc25c1e8a74c5d0087f
Sky Yong <jinghao0958@email.com>
2025-11-17 23:37:09 +0800

    product list & product details done

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,11 +7,11 @@ export default async function ProductsPage() {
     });
 
     return (
-        <div>
-            <h1>
+        <div className="pb-8">
+            <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
                 All Products
             </h1>
-            <ProductList products={products.data}></ProductList>
+            <ProductList products={products.data} />
         </div>
     )
 }
